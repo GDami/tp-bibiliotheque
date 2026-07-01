2c2fe0af2b08021638fba60b470529ddfebe5650
Damien <d.gervy@free.fr>
2026-07-01 14:42:08 +0200

    first commit, spring boot initializr

---
+++ backend/src/main/java/bibili/BibiliApplication.java
@@ -0,0 +1,13 @@
+package bibili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibiliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibiliApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/bibili/BibiliApplicationTests.java
@@ -0,0 +1,13 @@
+package bibili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibiliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
