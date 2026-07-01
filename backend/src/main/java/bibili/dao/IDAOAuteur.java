4369ac4010ecc12d1a0fbfb5e3abc27b77323824
Damien <d.gervy@free.fr>
2026-07-01 16:45:30 +0200

    moved idao to dao

--- backend/src/main/java/bibili/dao/IDAOAuteur.java
+++ backend/src/main/java/bibili/dao/IDAOAuteur.java
@@ -1,4 +1,4 @@
-package bibili.idao;
+package bibili.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,5 +7,8 @@ import org.springframework.data.repository.query.Param;
 import bibili.model.Auteur;
 
 public interface IDAOAuteur extends JpaRepository<Auteur,Integer> {
+
+    @Query("select a from Auteur a left join fetch a.livres where a.id = :idAuteur")
+    public Auteur findByIdWithLivres(@Param("idAuteur") Integer idAuteur);
 
 }
