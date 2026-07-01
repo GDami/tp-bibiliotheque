daac43c68530d2f5831955be37e918a2564adb30
bmoulard <baptiste.moulard@epitech.eu>
2026-07-01 16:01:54 +0200

    auteur restcontroler

---
+++ backend/src/main/java/bibili/restcontroller/AuteurRestController.java
@@ -0,0 +1,58 @@
+package bibili.restcontroller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import bibili.model.Auteur;
+import bibili.service.AuteurService;
+import bibili.view.Views;
+
+@RestController
+@RequestMapping("/api/auteur")
+// @CrossOrigin("http://localhost:4200")
+public class AuteurRestController {
+
+	@Autowired
+	AuteurService auteurSrv;
+
+	@GetMapping
+	@JsonView(Views.Auteur.class)
+	public List<Auteur> chercherTous() {
+		return auteurSrv.getAll();
+	}
+
+	@GetMapping("/{id}")
+	@JsonView(Views.Auteur.class)
+	public Auteur chercherParId(@PathVariable Integer id) {
+		return auteurSrv.getById(id);
+	}
+
+	@DeleteMapping("/{id}")
+	public void supprimer(@PathVariable Integer id) {
+		auteurSrv.delete(id);
+	}
+
+	@PostMapping
+	public Auteur ajouter(@RequestBody Auteur auteur) {
+		auteurSrv.insert(auteur);
+		return auteur;
+	}
+
+	@PutMapping("/{id}")
+	public Auteur modifier(@PathVariable Integer id, @RequestBody Auteur auteur) {
+		auteur.setId(id);
+		auteurSrv.update(auteur);
+		return auteur;
+	}
+}
