c7efef34deb1d645d45d3838c7d7eb10e344513d
Cécile <ccilejouanne@yahoo.fr>
2026-07-02 13:12:28 +0200

    Navigation+Routes

--- frontend/bibliotheque/src/app/app.routes.ts
+++ frontend/bibliotheque/src/app/app.routes.ts
@@ -1,14 +1,43 @@
 import { Routes } from '@angular/router';
-import { LoginPage } from './page/login/login';
+import { HomePage } from './page/home-page/home-page';
+import { LoginPage } from './page/login-page/login-page';
+import { authGuard } from './guard/auth-guard';
+import { LivrePage } from './page/livre-page/livre-page';
+import { AuteurPage } from './page/auteur-page/auteur-page';
 import { CollectionPage } from './page/collection-page/collection-page';
+import { EditeurPage } from './page/editeur-page/editeur-page';
 
 export const routes: Routes = [
   {
-    path: 'collection',
-    component: CollectionPage
+    path: 'home',
+    component: HomePage,
+    canActivate: [ authGuard ]
   },
+
   {
-    path: 'login',
-    component: LoginPage
-  }
+    path: 'livre',
+    component: LivrePage,
+    canActivate: [ authGuard ]
+  },
+
+  {
+    path: 'auteur',
+    component: AuteurPage,
+    canActivate: [ authGuard ]
+  },
+
+    {
+    path: 'collection',
+    component: CollectionPage,
+    canActivate: [ authGuard ]
+  },
+
+    {
+    path: 'editeur',
+    component: EditeurPage,
+    canActivate: [ authGuard ]
+  },
+
+  { path: 'login', component: LoginPage },
+  { path: '', redirectTo: 'navigation', pathMatch: 'full' }
 ];
